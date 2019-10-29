caa740099b4dd74949dd019106678da36fb3168f
kiki <772765729@qq.com>
2019-10-29 15:16:28 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="panel">
+      <div>第{{n}}手</div>
+      <div>{{result===null?'':`胜利者：${result}`}}</div>
+    </div>
+    <div class="chess">
+      <div class="row">
+        <Cell @click="onClickCell(0,$event)" :n="n" />
+        <Cell @click="onClickCell(1,$event)" :n="n" />
+        <Cell @click="onClickCell(2,$event)" :n="n" />
+      </div>
+      <div class="row">
+        <Cell @click="onClickCell(3,$event)" :n="n" />
+        <Cell @click="onClickCell(4,$event)" :n="n" />
+        <Cell @click="onClickCell(5,$event)" :n="n" />
+      </div>
+      <div class="row">
+        <Cell @click="onClickCell(6,$event)" :n="n" />
+        <Cell @click="onClickCell(7,$event)" :n="n" />
+        <Cell @click="onClickCell(8,$event)" :n="n" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Cell from "./Cell";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  components: { Cell },
+  data() {
+    return {
+      n: 0,
+      map: [[null, null, null], [null, null, null], [null, null, null]],
+      result: null
+    };
+  },
+  methods: {
+    onClickCell(i, text) {
+      this.map[Math.floor(i / 3)][i % 3] = text;
+      this.n = this.n + 1;
+      this.tell();
+    },
+    tell() {
+      for (let i = 0; i < 3; i++) {
+        if (
+          this.map[i][0] !== null &&
+          this.map[i][0] === this.map[i][1] &&
+          this.map[i][1] === this.map[i][2]
+        ) {
+          this.result = this.map[i][0];
+        }
+      }
+      for (let j = 0; j < 3; j++) {
+        if (
+          this.map[0][j] !== null &&
+          this.map[0][j] === this.map[1][j] &&
+          this.map[1][j] === this.map[2][j]
+        ) {
+          this.result = this.map[0][j];
+        }
+      }
+      if (
+        this.map[0][0] !== null &&
+        this.map[0][0] === this.map[1][1] &&
+        this.map[1][1] === this.map[2][2]
+      ) {
+        this.result = this.map[0][0];
+      }
+      if (
+        this.map[0][2] !== null &&
+        this.map[0][2] === this.map[1][1] &&
+        this.map[1][1] === this.map[2][0]
+      ) {
+        this.result = this.map[0][2];
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+}
+.row {
+  display: flex;
+}
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.panel {
+  margin-right: 30px;
+  background-color: bisque;
+  width: 100px;
+  height: 100px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 20px;
 }
 </style>
---
+++ src/Cell.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-on:click="onClickSelf" class="cell">
+      <template v-if="a">{{text}}</template>
+      <template v-else></template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["n"],
+  data() {
+    return { a: false, text: null };
+  },
+  methods: {
+    onClickSelf() {
+      if (this.text === null) {
+        this.a = true;
+        this.text = this.n % 2 === 0 ? "x" : "o";
+        this.$emit("click", this.text);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.cell {
+  border: 1px solid black;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 60px;
+}
+</style>
